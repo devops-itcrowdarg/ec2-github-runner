be5f657e985ec62e089241f80cb69d693274da1d
Alexandre Pereira <apereira@itcrowdarg.com>
2024-05-16 14:51:47 -0300

    add worker label for new runner instance

--- src/aws.js
+++ src/aws.js
@@ -13,7 +13,7 @@ function buildUserDataScript(githubRegistrationToken, label) {
       `echo "${config.input.preRunnerScript}" > pre-runner-script.sh`,
       'source pre-runner-script.sh',
       'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.getGitHubApiRepoPath()} --token ${githubRegistrationToken} --labels ${label}`,
+      `./config.sh --url https://github.com/${config.getGitHubApiRepoPath()} --token ${githubRegistrationToken} --labels ${label},worker`,
       './run.sh',
     ];
   } else {
@@ -26,7 +26,7 @@ function buildUserDataScript(githubRegistrationToken, label) {
       `curl -O -L https://github.com/actions/runner/releases/download/v${config.input.runnerVersion}/actions-runner-linux-$RUNNER_ARCH-${config.input.runnerVersion}.tar.gz`,
       `tar xzf ./actions-runner-linux-$RUNNER_ARCH-${config.input.runnerVersion}.tar.gz`,
       'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.getGitHubApiRepoPath()} --token ${githubRegistrationToken} --labels ${label}`,
+      `./config.sh --url https://github.com/${config.getGitHubApiRepoPath()} --token ${githubRegistrationToken} --labels ${label},worker`,
       './run.sh',
     ];
   }
