fba07e56b53fa828752ee6b9e7e277a34a23ceb1
Alexandre Pereira <apereira@itcrowdarg.com>
2024-09-05 16:46:42 -0300

    Fix remove keypair

--- dist/index.js
+++ dist/index.js
@@ -62847,7 +62847,6 @@ async function startEc2Instance(label, githubRegistrationToken) {
     SecurityGroupIds: [config.input.securityGroupId],
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
-    KeyName: "github-runner",
     BlockDeviceMappings: [
       {
         DeviceName: "/dev/sda1", 
